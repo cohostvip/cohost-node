0cc452b72f1de2def01a7b03bed1172a97376bdd
Segev Shmueli <cj@segevs.com>
2025-04-14 14:02:59 -0400

    feat: init commit #2

--- types/models/ActiveOfferingStatus.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export enum ActiveOfferingStatus {
-    HIDDEN = 'hidden',
-    LIVE = 'live',
-    SOLD_OUT = 'sold-out',
-}
